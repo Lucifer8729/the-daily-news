9ebdc0cfa1dac76168feaa79974ae754101a61e3
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-09-22 16:12:41 +0530

    setting up file structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Routes from "./routes";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/styles.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Routes />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
